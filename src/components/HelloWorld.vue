daefa48d499e464d9b5fe1518a3d757ded20b050
Douglas <dz@bitzend.net>
2021-07-19 11:46:23 -0600

    formatting  cganes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Welcome to Naturistic,  your online resource for high definition nature photos
         </h1>
 
         <p class="subheading font-weight-regular">
