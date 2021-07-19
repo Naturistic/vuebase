4bfac16b14f2748426f49fc00c6069dfb5684cba
Douglas <dz@bitzend.net>
2021-07-19 12:02:10 -0600

    remove discord and change  heading

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,18 +11,10 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
+        <h2 class="display-2 font-weight-bold mb-3">
           Welcome to Naturistic,  your online resource for high definition nature photos
-        </h1>
+        </h2>
 
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
       </v-col>
 
       <v-col
