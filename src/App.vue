5882800701577ee7010c1e28f14bcb7b496d4f36
Douglas <dz@bitzend.net>
2021-07-20 10:44:43 -0600

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark >
       <div class="d-flex align-center">
-        <v-img
-          alt="Naturistic Logo"
-          class="shrink mr-2"
-          contain
-          :src="require('./assets/favacon.png')"
-          transition="scale-transition"
-          width="40"
-        />
-          <v-btn fab color="#0097A7" small><a href="#" class="link" target="_blank"><v-icon style="color: white;">mdi-home</v-icon></a></v-btn>
 
+          <v-btn fab color="#0097A7" small>
+            <a href="#" class="link" target="_blank">
+              <v-icon style="color: white;">mdi-home</v-icon>
+            </a>
+          </v-btn>
       </div>
-
       <v-spacer></v-spacer>
-
     </v-app-bar>
 
     <v-main>
