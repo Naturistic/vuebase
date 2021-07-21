34b731b0dfc86ac676272959b18fa129af78f158
Douglas <dz@bitzend.net>
2021-07-21 06:44:44 -0600

    merge  about page  to test for  errors

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,90 @@
 <template>
   <div class="about">
-    <h1>This is the Naturistic About Page</h1>
+     <v-card
+     max-width= "1000"
+     id="developers"
+     elvation="14"
+     class="mx-auto"
+
+     >
+     <v-card-title p="3" class="text-h5"><strong>The Developers</strong></v-card-title>
+     <v-spacer></v-spacer>
+     <v-container>
+     <v-row dense>
+         <v-col cols="12">
+     <v-card
+     p="7"
+     color="cyan darken-4"
+     class="white--text"
+     >
+         <v-card-title class="text-h6">Daniel Urias</v-card-title>
+         <div class="profile-section">
+             <v-card-subtitle class="about-developer" color="#006064">
+                 Lorem ipsum dolor sit amet,
+                 consectetur adipiscing elit, sed do eiusmod
+                 tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation
+                 ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 <br>
+                 <br>
+                 <v-btn fab color="#0097A7" class="mr-icon" small><a href="https://www.linkedin.com/in/daniel-urias-aa89451b2" class="link" target="_blank"><v-icon style="color: white;">mdi-linkedin</v-icon></a></v-btn>
+                 <v-btn fab color="#0097A7" small><a href="#" class="link" target="_blank"><v-icon style="color: white;">mdi-github</v-icon></a></v-btn>
+             </v-card-subtitle>
+             <v-card-subtitle><v-img contain src="../images/sun_mountain.jpg" width="300"></v-img></v-card-subtitle>
+
+         </div>
+         </v-card>
+     </v-col>
+     <v-spacer></v-spacer>
+     <v-col cols="12">
+     <v-card
+     p="7"
+     color="#006064"
+     class="white--text">
+
+         <v-card-title class="text-h6">Kalicia Adams</v-card-title>
+         <div class="profile-section">
+             <v-card-subtitle> <v-img contain src="../images/kalicia.jpg" width="300"></v-img></v-card-subtitle>
+             <v-card-subtitle class="about-developer">Lorem ipsum dolor sit amet,
+             consectetur adipiscing elit, sed do eiusmod
+             tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 <v-spacer></v-spacer>
+                 <br>
+                 <v-btn fab color="#0097A7" class="mr-icon" small><a href="https://www.linkedin.com/in/kalicia-adams-2b489a207" class="link" target="_blank"><v-icon style="color: white;">mdi-linkedin</v-icon></a></v-btn>
+                 <v-btn fab color="#0097A7" small><a href="https://github.com/kally1009" class="link" target="_blank"><v-icon style="color: white;">mdi-github</v-icon></a></v-btn>
+             </v-card-subtitle>
+
+         </div>
+       </v-card>
+     </v-col>
+
+     <v-col col="12">
+     <v-card
+     p="7"
+     color="#006064"
+     class="white--text"
+     >
+         <v-card-title class="text-h6">Doug Osborn</v-card-title>
+         <div class="profile-section">
+         <v-card-subtitle class="about-developer">Lorem ipsum dolor sit amet,
+             consectetur adipiscing elit, sed do eiusmod
+             tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <br>
+              <br>
+              <v-btn fab color="#0097A7" class="mr-icon" small><a href="https://www.linkedin.com/in/opensimian" class="link" target="_blank"><v-icon style="color: white;">mdi-linkedin</v-icon></a></v-btn>
+              <v-btn fab color="#0097A7" small><a href="#" class="link" target="_blank"><v-icon style="color: white;">mdi-github</v-icon></a></v-btn>
+             </v-card-subtitle>
+              <v-card-subtitle><v-img contain src="../images/lake.jpg" width="300"></v-img></v-card-subtitle>
+         </div>
+     </v-card>
+ </v-col>
+</v-row>
+</v-container>
+</v-card>
+
   </div>
 </template>
