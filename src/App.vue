cc7f7c29fe9a226ad4c4754880292d7c0cc64db0
Douglas <dz@bitzend.net>
2021-07-23 10:05:11 -0600

    all colors to theme

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-app-bar app color='primary' dark >
       <div class="d-flex align-center">
 
-          <v-btn fab color="#0097A7" small>
+          <v-btn fab color='secondary' small>
             <router-link  style="text-decoration: none; color: inherit;" to= '/' >
               <v-icon style="color: white;">mdi-home</v-icon>
             </router-link>
