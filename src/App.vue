29564b0807717eefdefe6070eaeee00c85284ede
Douglas <dz@bitzend.net>
2021-07-23 10:43:49 -0600

    try v-if on button

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
   <v-app>
     <v-app-bar app color='primary' dark >
       <div class="d-flex align-center">
-
           <v-btn fab color='secondary' small>
             <router-link  style="text-decoration: none; color: inherit;" to= '/' >
               <v-icon style="color: white;">mdi-home</v-icon>
             </router-link>
+          </v-btn>
+          <v-btn v-if= $vuetify.theme.dark
+           @click="$vuetify.theme.dark  = false">Light
+           v-else
+           @click="$vuetify.theme.dark = true">Dark
           </v-btn>
       </div>
       <v-spacer></v-spacer>
