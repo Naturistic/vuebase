2bc91539d2b7e09893ed83931da3a37368acde7e
Douglas <dz@bitzend.net>
2021-07-25 12:13:33 -0600

    reduce divs

--- src/App.vue
+++ src/App.vue
@@ -4,83 +4,77 @@
 
     <v-app-bar app color='primary' dark >
 
-      <div class="d-flex align-center">
+      <div class="d-flex align-center" v-if=$vuetify.theme.dark>
 
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
+        <v-btn fab color='secondary' small>
+          <router-link  style="text-decoration: none;" to= '/' >
+            <v-icon style="color: black;">mdi-home-outline</v-icon>
+          </router-link>
+        </v-btn>
 
+        <v-toolbar-title>Naturistic</v-toolbar-title>
 
-          <div v-if=$vuetify.theme.dark>
+        <v-btn fab color='secondary' small @click="$vuetify.theme.dark = false">
+          <v-icon style="color: black;">mdi-brightness-5</v-icon>
+        </v-btn>
 
-            <v-btn fab color='secondary' small>
-              <router-link  style="text-decoration: none;" to= '/' >
-                <v-icon style="color: black;">mdi-home-outline</v-icon>
-              </router-link>
-            </v-btn>
+        <v-btn fab color='secondary' small>
+          <router-link  style="text-decoration: none;" to= '/about' >
+            <v-icon style="color: black;">mdi-help</v-icon>
+          </router-link>
+        </v-btn>
 
-            <v-toolbar-title>Naturistic</v-toolbar-title>
+        <v-btn fab color='secondary' small>
+          <router-link  style="text-decoration: none;" to= '/gallery' >
+            <v-icon style="color: black;">mdi-image-multiple</v-icon>
+          </router-link>
+        </v-btn>
 
-            <v-btn fab color='secondary' small @click="$vuetify.theme.dark = false">
-              <v-icon style="color: black;">mdi-brightness-5</v-icon>
-            </v-btn>
-
-            <v-btn fab color='secondary' small>
-              <router-link  style="text-decoration: none;" to= '/about' >
-                <v-icon style="color: black;">mdi-help</v-icon>
-              </router-link>
-            </v-btn>
-
-            <v-btn fab color='secondary' small>
-              <router-link  style="text-decoration: none;" to= '/gallery' >
-                <v-icon style="color: black;">mdi-image-multiple</v-icon>
-              </router-link>
-            </v-btn>
-
-            <v-btn fab color='secondary' small>
-              <router-link  style="text-decoration: none;" to= '/cart' >
-                <v-icon style="color: black;">mdi-cart-outline</v-icon>
-              </router-link>
-            </v-btn>
-
-          </div>
-
-          <div v-else>
-
-            <v-btn fab color='secondary' small>
-              <router-link  style="text-decoration: none;" to= '/' >
-                <v-icon style="color: white;">mdi-home-outline</v-icon>
-              </router-link>
-            </v-btn>
-
-            <v-toolbar-title>Naturistic</v-toolbar-title>            
-
-            <v-btn fab color='secondary' small @click="$vuetify.theme.dark = true">
-              <v-icon style="color: white;">mdi-brightness-3</v-icon>
-            </v-btn>
-
-            <v-btn fab color='secondary' small>
-            <router-link  style="text-decoration: none;" to= '/about' >
-              <v-icon style="color: white;">mdi-help</v-icon>
-            </router-link>
-            </v-btn>
-
-            <v-btn fab color='secondary' small>
-              <router-link  style="text-decoration: none;" to= '/gallery' >
-                <v-icon style="color: white;">mdi-image-multiple</v-icon>
-              </router-link>
-            </v-btn>
-
-            <v-btn fab color='secondary' small>
-            <router-link  style="text-decoration: none;" to= '/cart' >
-              <v-icon style="color: white;">mdi-cart-outline</v-icon>
-            </router-link>
-            </v-btn>
-
-          </div>
+        <v-btn fab color='secondary' small>
+          <router-link  style="text-decoration: none;" to= '/cart' >
+            <v-icon style="color: black;">mdi-cart-outline</v-icon>
+          </router-link>
+        </v-btn>
 
       </div>
 
+      <div class="d-flex align-center" v-else>
 
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-btn fab color='secondary' small>
+          <router-link  style="text-decoration: none;" to= '/' >
+            <v-icon style="color: white;">mdi-home-outline</v-icon>
+          </router-link>
+        </v-btn>
+
+        <v-toolbar-title>Naturistic</v-toolbar-title>
+
+        <v-btn fab color='secondary' small @click="$vuetify.theme.dark = true">
+          <v-icon style="color: white;">mdi-brightness-3</v-icon>
+        </v-btn>
+
+        <v-btn fab color='secondary' small>
+        <router-link  style="text-decoration: none;" to= '/about' >
+          <v-icon style="color: white;">mdi-help</v-icon>
+        </router-link>
+        </v-btn>
+
+        <v-btn fab color='secondary' small>
+          <router-link  style="text-decoration: none;" to= '/gallery' >
+            <v-icon style="color: white;">mdi-image-multiple</v-icon>
+          </router-link>
+        </v-btn>
+
+        <v-btn fab color='secondary' small>
+        <router-link  style="text-decoration: none;" to= '/cart' >
+          <v-icon style="color: white;">mdi-cart-outline</v-icon>
+        </router-link>
+        </v-btn>
+
+      </div>
 
     </v-app-bar>
 
